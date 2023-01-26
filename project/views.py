edfe3f66371245ab06aec0642bf486dc6c83b7b2
Brandon Harding <brandonaharding@gmail.com>
2023-01-26 10:53:34 -0600

    Add content to the About page

--- project/views.py
+++ project/views.py
@@ -1,12 +1,22 @@
 from flask import Blueprint, render_template, request, redirect
 from .models import tests
+import datetime
 
 views = Blueprint("views", __name__)
 
 @views.route('/', methods=['GET'])
 def index():
     test_names = tests.query.all()
-    return render_template('index.html', tests=test_names)
+    date_choice = datetime.datetime.now().strftime("%Y-%m-%d")
+    return render_template('index.html', tests=test_names, date=date_choice)
+
+@views.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@views.route('/team', methods=['GET'])
+def team():
+    return render_template('team.html')
 
 @views.route('/help', methods=['GET'])
 def help():
