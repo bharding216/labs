dd4ea8e9cd14c00f1eedf417b975255ad6a09020
Brandon Harding <brandonaharding@gmail.com>
2023-02-05 11:34:01 -0600

    Add lab/user login/logout functionality

--- project/auth.py
+++ project/auth.py
@@ -19,9 +19,12 @@ def lab_login():
             if check_password_hash(lab.password, password):
                 login_user(lab, remember=False)
                 session.permanent = True
+                flash('Login successful!', category='success')
                 return redirect(url_for('views.index'))
             else:
-                return redirect(url_for('views.about'))
+                flash('Incorrect password. Please try again.', category='error')
+        else:
+            flash('That email is not associated with an account.', category='error')
 
     return render_template('lab_login.html', user=current_user)
 
@@ -33,65 +36,80 @@ def individual_login():
         email = request.form["email"]
         password = request.form["password"]
 
-        individual_email = labs_login.query.filter_by(email=email).first()
+        individual_email = individuals_login.query.filter_by(email=email).first()
         if individual_email:
-            if check_password_hash(labs_login.password, password):
-                flash('Successfully logged in!', category='success')
+            if check_password_hash(individual_email.password, password):
                 login_user(individual_email, remember=False)
                 session.permanent = True
+                flash('Login successful!', category='success')
                 return redirect(url_for('views.index'))
             else:
-                flash('Password is incorrect. Please try again.', 
-                    category='error')
+                flash('Incorrect password. Please try again.', category='error')
         else:
-            flash('That username does not exist. Please try again or contact \
-                your system administrator.', 
-                category='error')
-
-    flash('Please log in to continue.', category='error')       
+            flash('That email is not associated with an account.', category='error')
 
     return render_template('user_login.html', user=current_user)
 
 
-@auth_bp.route("/signup", methods=['GET', 'POST'])
+# New lab signup
+@auth_bp.route("/lab_signup", methods=['GET', 'POST'])
 def lab_signup():
     if request.method == "POST":
-        username = request.form['username']
-        #lab_name = request.form['lab_name']
-        #lab_email = request.form['email']
-        #phone = request.form['phone']
+        lab_name = request.form['name']
+        lab_email = request.form['email']
+        lab_phone = request.form['phone']
         password1 = request.form['password1']
         password2 = request.form['password2']
 
-        #email_exists = lab_login.query.filter_by(lab_email=lab_email).first()
-        #if email_exists:
-        #    flash('Email is already in use. \
-        #    Choose another email.', category='error')
-
-
         if password1 != password2:
             flash('Passwords do not match.', category='error')
-            return redirect(url_for('views.about'))
 
         else:
             hashed_password = generate_password_hash(password1)
-            new_user = labs_login(lab_name=username, password=hashed_password, phone=username, email=username)
-            db.session.add(new_user)
+            new_lab = labs_login(lab_name=lab_name, password=hashed_password, phone=lab_phone, email=lab_email)
+            db.session.add(new_lab)
             db.session.commit()
-            flash("Thanks for signing up!", category='success')
+            flash('Lab successfully added to database.', category='success')
             return redirect(url_for('views.index'))
-
 
     return render_template('lab_signup.html', user=current_user)
 
 
+# New user signup
+@auth_bp.route("/user_signup", methods=['GET', 'POST'])
+def user_signup():
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        phone = request.form['phone']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if password1 != password2:
+            flash('Passwords do not match.', category='error')
+
+        else:
+            hashed_password = generate_password_hash(password1)
+            new_user = individuals_login(first_name=first_name, last_name=last_name, password=hashed_password, phone=phone, email=email)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User successfully added to database.', category='success')
+            return redirect(url_for('views.index'))
+
+    return render_template('user_signup.html', user=current_user)
 
 
 
 
-@auth_bp.route("/lab_logout")
+
+
+
+
+@auth_bp.route("/logout")
 @login_required
-def lab_logout():
+def logout():
+    flash('User successfully logged out', category='success')
     logout_user()
 
-    return redirect(url_for('auth.lab_login'))
+    return redirect(url_for('views.index'))
