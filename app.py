f075a9b27c886a3c825a5c0ccaf05affc35a477d
Brandon Harding <brandonaharding@gmail.com>
2023-02-21 11:33:43 -0600

    updated app.py to deploy

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from project import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=2000, debug=True)
-    #app.run()
+    #app.run(host='localhost', port=2000, debug=True)
+    app.run()
