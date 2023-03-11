b6f1132e600f2715cc46bad1d1d7dee2fa0d5580
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-03-11 13:34:21 -0600

    added full url to sitemap.xml

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-from flask import url_for, current_app
+from flask import url_for, current_app, request
 
 # helpers.py
 
@@ -8,10 +8,13 @@ def my_enumerate(seq, start=0):
 
 def generate_sitemap():
     with current_app.app_context():
+        base_url = request.host_url
         sitemap_xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
         sitemap_xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
         for rule in current_app.url_map.iter_rules():
             if "GET" in rule.methods and not bool(rule.arguments):
-                sitemap_xml += f'\t<url><loc>{url_for(rule.endpoint)}</loc></url>\n'
+                endpoint = url_for(rule.endpoint)
+                url = base_url.rstrip('/') + endpoint
+                sitemap_xml += f'\t<url><loc>{url}</loc></url>\n'
         sitemap_xml += '</urlset>\n'
         return sitemap_xml
