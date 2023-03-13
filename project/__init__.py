a748fe7a6e1aba603d0e2689fdadd3dd503f0342
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-03-12 20:08:05 -0500

    added contact form email function

--- project/__init__.py
+++ project/__init__.py
@@ -39,9 +39,9 @@ def create_app():
 
 
     # Mail config settings:
-    app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
-    app.config['MAIL_PORT'] = 2525
-    app.config['MAIL_USERNAME'] = 'c14de80d53a0d6'
+    app.config['MAIL_SERVER']='live.smtp.mailtrap.io'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USERNAME'] = 'api'
     app.config['MAIL_PASSWORD'] = os.getenv('mail_password')
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USE_SSL'] = False
--- project/contact.py
+++ project/contact.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 from flask_login import login_required, current_user
 from .models import tests, labs, labs_tests
+from flask_mail import Mail, Message
+from project import mail
 import datetime
 import phonenumbers
 
 contact_bp = Blueprint('contact', __name__, 
-    template_folder='contact_templates', static_folder='contact_static')
+    template_folder='contact_templates', static_folder='static')
 
 
 
@@ -13,18 +15,36 @@ contact_bp = Blueprint('contact', __name__,
 def contact_function():
     if request.method == 'POST':
         first_name = request.form['first_name']
-        email = request.form['email']
+        last_name = request.form['last_name']
+        user_email = request.form['email']
         phone = request.form['phone']
-        message = request.form['message']
+        user_message = request.form['message']
+
+        msg = Message('New Contact Form Submission',
+                        sender = 'hello@unifiedsl.com',
+                        recipients = ['team@unifiedsl.com'
+                                      ]
+                        )
+        
+        msg.html = render_template('contact_email.html',
+                                   first_name = first_name,
+                                   last_name = last_name,
+                                   user_email = user_email,
+                                   phone = phone,
+                                   user_message = user_message,
+                                   customer_type = 'Customer')
+
+        mail.send(msg)
 
         return render_template('contact_success.html', 
                                 first_name = first_name,
-                                email = email, 
+                                email = user_email, 
                                 phone = phone, 
-                                message = message,
+                                message = user_message,
                                 user = current_user)
 
-    return render_template('contact.html', user = current_user)
+    return render_template('contact.html', 
+                           user = current_user)
 
 
 
@@ -34,7 +54,30 @@ def contact_function():
 def lab_contact():
     if request.method == 'POST':
         first_name = request.form['first_name']
+        last_name = request.form['last_name']
         lab_name = request.form['lab_name']
+        title = request.form['title']
+        user_email = request.form['email']
+        phone = request.form['phone']
+        user_message = request.form['message']
+
+        msg = Message('New Contact Form Submission',
+                        sender = 'hello@unifiedsl.com',
+                        recipients = ['team@unifiedsl.com'
+                                      ]
+                        )
+        
+        msg.html = render_template('contact_email.html',
+                                   first_name = first_name,
+                                   last_name = last_name,
+                                   lab_name = lab_name,
+                                   title = title,
+                                   user_email = user_email,
+                                   phone = phone,
+                                   user_message = user_message,
+                                   customer_type = 'Provider')
+
+        mail.send(msg)
 
         return render_template('lab_contact_success.html',
                                user = current_user,
