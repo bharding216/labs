dec02e29904f81ee7e554bd80b8cfe444bffb8cc
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2023-04-30 15:30:06 -0500

    New hide_show password feature

--- project/contact.py
+++ project/contact.py
@@ -92,7 +92,79 @@ def lab_contact():
                            user = current_user)
 
 
+@contact_bp.route('/lab_contact_question', methods=['GET', 'POST'])
+def lab_contact_question():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        lab_name = request.form['lab_name']
+        title = request.form['title']
+        user_email = request.form['email']
+        phone = request.form['phone']
+        user_message = request.form['message']
+
+        msg = Message('New Contact Form Submission',
+                        sender = ("USL Contact Form", 'hello@unifiedsl.com'),
+                        recipients = ['team@unifiedsl.com,'
+                                      ]
+                        )
+        
+        msg.html = render_template('contact_email.html',
+                                   first_name = first_name,
+                                   last_name = last_name,
+                                   lab_name = lab_name,
+                                   title = title,
+                                   user_email = user_email,
+                                   phone = phone,
+                                   user_message = user_message,
+                                   customer_type = 'Provider')
+        try:
+            mail.send(msg)
+
+            return render_template('lab_contact_success.html',
+                                user = current_user,
+                                first_name = first_name,
+                                lab_name = lab_name
+                                )
+
+        except Exception as e:
+            return str(e)
+
+    return render_template('lab_contact_question.html', 
+                           user = current_user)
+
 @contact_bp.route('/new_test_request', methods=['GET', 'POST'])
 def new_test_request():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        user_email = request.form['email']
+        phone = request.form['phone']
+        user_message = request.form['message']
+
+        msg = Message('New Test Inquiry',
+                        sender = ("USL New Test Inquiry", 'hello@unifiedsl.com'),
+                        recipients = ['team@unifiedsl.com'
+                                      ]
+                        )
+        
+        msg.html = render_template('contact_inquiry_email.html',
+                                   first_name = first_name,
+                                   last_name = last_name,
+                                   user_email = user_email,
+                                   phone = phone,
+                                   user_message = user_message
+                                   )
+
+        mail.send(msg)
+
+        return render_template('contact_success.html', 
+                                first_name = first_name,
+                                email = user_email, 
+                                phone = phone, 
+                                message = user_message,
+                                user = current_user)
+
+
     return render_template('contact_new_test_request.html',
                            user = current_user)
